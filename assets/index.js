a3e961b497a786b8150e50b687fa029374ed40c9
tierney03 <tierney_03@mac.com>
2022-09-08 10:48:17 -0500

    javascript work

--- assets/index.js
+++ assets/index.js
@@ -1,26 +1,17 @@
 const form = document.querySelector('form')
 const ul = document.querySelector('ul')
 const button = document.querySelector('.searchBtn')
-const input = document.getElementById('lat','lon')
+const input = document.getElementById('city')
+const clear = document.querySelector('.clear')
+const inputValue = document.querySelector('.inputValue')
 
 let itemsArray = []
 
-localStorage.setItem('items', JSON.stringify(itemsArray))
-const data = JSON.parse(localStorage.getItem('items'))
-
-itemsArray.push(input.value)
-localStorage.setItem('items', JSON.stringify(itemsArray))
-
-data.forEach((item) => {
-  liCity(item)
-})
-
-button.addEventListener('click', function () {
-  localStorage.clear()
-  while (ul.firstChild) {
-    ul.removeChild(ul.firstChild)
-  }
-})
+function saveCity(newCity) {
+  let data = JSON.parse(localStorage.getItem('saved-cities')) || []
+  data.push(newCity)
+  localStorage.setItem('saved-cities', JSON.stringify(data))
+}
 
 const liCity = (text) => {
     const li = document.createElement('li')
@@ -28,10 +19,27 @@ const liCity = (text) => {
     ul.appendChild(li)
   }
 
-  form.addEventListener('searchBtn', function (e) {
+function displayCity() {
+  let data = JSON.parse(localStorage.getItem('saved-cities')) || []
+  ul.innerText = ''
+  data.forEach((city) => {
+  liCity(city)
+})
+}
+displayCity()
+
+clear.addEventListener('click', function () {
+  localStorage.clear()
+  displayCity()
+  }
+)
+
+  button.addEventListener('click', function (e) {
     e.preventDefault()
   
-    liCity(input.value)
+    saveCity(input.value)
     input.value = ''
+    displayCity()
   })
 
+  // fetch('https://api.openweathermap.org/data/3.0/onecall?q='+inputValue.value+&appid='d4adb16a540d05747383168aac8cb7cd)
